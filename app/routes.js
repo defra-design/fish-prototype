86b95bc29ec851f29716200c83e4e4c6d33c435f
Steve Murphy <stephen.murphy@defra.gov.uk>
2021-01-11 16:40:12 +0000

    less-paper change options 1st part

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,18 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// GAFL routing for what the licence type is (3 rods means only 12 months is valid)
+router.post('/views/gafl/route-licence-type', function (req, res) {
+
+  let licenceType = req.session.data['licence-type']
+
+  if (licenceType === 'Trout and coarse, up to 3 rods') {
+    res.redirect('views/gafl/licence-summary')
+  } else {
+    res.redirect('views/gafl/licence-length')
+  }
+})
+
 
 // GAFL routing for the question about where you want your licence to be sent
 router.post('/views/gafl/licence-by-question', function (req, res) {
