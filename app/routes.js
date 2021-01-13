d3d99e9c5d5af945a126ce27c3eebcb497cdac68
Steve Murphy <stephen.murphy@defra.gov.uk>
2021-01-13 13:51:48 +0000

    less-paper tidy up and content changes

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,18 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 // the bit between '' is the reference for this code, linked to from the form action
+router.post('/check-licence-length', function (req, res) {
+  // Make a variable from session data
+  let licenceType = req.session.data['licence-type']
+  // route depending on value
+  if (licenceType === 'Trout and coarse, up to 3 rods') {
+    res.redirect('gafl/contact-summary')
+  } else {
+    res.redirect('gafl/licence-length')
+  }
+})
+
+// routing for digital/paper licence
 router.post('/check-licence-type', function (req, res) {
   // Make a variable from session data
   let licenceOption = req.session.data['licence-option']
@@ -16,12 +28,13 @@ router.post('/check-licence-type', function (req, res) {
   }
 })
 
+// routing for changed digital/paper licence
 router.post('/check-change-licence-type', function (req, res) {
   // Make a variable from session data
   let licenceOption = req.session.data['licence-option']
   // route depending on value
   if (licenceOption === 'digital') {
-    res.redirect('gafl/licence-by')
+    res.redirect('gafl/change-licence-by')
   } else {
     res.redirect('gafl/contact-summary')
   }
